5b2b9409298c43b3cff1830b52f9d37fef2f7095
ayavito <3104xsmk@gmail.com>
2026-07-15 14:30:00 +0000

    マス画面の確認

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("九九ナンプレ開始");
+function startGame() {
+  document.querySelector(".container").style.display = "none";
+  document.getElementById("game").style.display = "block";
+
+  createBoard();
+}
+
+
+function createBoard() {
+  const board = document.getElementById("board");
+
+  board.innerHTML = "";
+
+  for (let i = 0; i < 81; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    board.appendChild(cell);
+  }
+}
